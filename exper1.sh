b45fb9329ae508a6d5d73ac4e47ec2ea68b111a3
CianOpening <cian@opening.io>
2019-07-17 09:14:05 +0100

    experiment

--- exper1.sh
+++ exper1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "this is a test to see if credentials are displayed"
-git pull https://$1:$2@github.com/CianOpening/jenkins-test.git
+user=$1
+password=$2
+git pull https://$user:$password@github.com/CianOpening/jenkins-test.git
